13e28c6fb4c7b9e00f6277cbdd45eafdda98178d
me <me@email.com>
2017-10-04 17:33:07 -0500

    lab 6

---
+++ app/src/main/java/com/example/hl4350hb/staticbluegreenfragment/GreenFragment.java
@@ -0,0 +1,19 @@
+package com.example.hl4350hb.staticbluegreenfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hl4350hb on 10/4/17.
+ */
+
+public class GreenFragment extends Fragment {
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.green_fragment, container, false);
+    }
+}
