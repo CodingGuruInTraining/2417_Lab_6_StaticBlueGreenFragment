05755d0717c6767863c2d741a875ab3ab74df278
me <me@email.com>
2017-10-04 18:10:40 -0500

    changed to dynamic

--- app/src/main/java/com/example/hl4350hb/staticbluegreenfragment/MainActivity.java
+++ app/src/main/java/com/example/hl4350hb/staticbluegreenfragment/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.hl4350hb.staticbluegreenfragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Create fragment instances
+    private GreenFragment greenFragment = new GreenFragment();
+    private BlueFragment blueFragment = new BlueFragment();
+
+    // Labels for currently displayed fragment
+    private static final String BLUE_TAG = "BLUE";
+    private static final String GREEN_TAG = "GREEN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        updateFragment();
+
+        // Add click listener to main window
+        // android.R.id.content is a built-in reference to Activity's main UI component
+        View v = findViewById(android.R.id.content);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFragment();
+            }
+        });
+    }
+
+    private void updateFragment() {
+        // Swaps fragments
+
+        // Gets reference to this Activity's FragmentManager
+        FragmentManager fm = getFragmentManager();
+        // Request the fm begins a Fragment Transaction
+        FragmentTransaction ft = fm.beginTransaction();
+
+        // If blue fragment is shown, replace with green fragment
+        if (fm.findFragmentByTag(BLUE_TAG) != null) {
+            ft.replace(android.R.id.content, greenFragment, GREEN_TAG);
+        }
+
+        // If green fragment is shown, replace with blue fragment
+        else {
+            ft.replace(android.R.id.content, blueFragment, BLUE_TAG);
+        }
+
+        // Committing is necessary or no changes are made
+        ft.commit();
     }
 }
